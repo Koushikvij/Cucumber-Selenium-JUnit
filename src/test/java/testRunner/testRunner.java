efd7ecc15e3ae52387c6869e1f63ff4146cc50bd
Koushikvij <Koushikvij@gmail.com>
2024-05-09 09:49:00 -0400

    Initial Commit

---
+++ src/test/java/testRunner/testRunner.java
@@ -0,0 +1,19 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/resources/features",
+		glue ="stepDefinitions",
+		monochrome=true,
+		plugin= {"pretty","html:target/cucumber.html",
+         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+         "rerun:target/rerun.txt"
+        })
+public class testRunner {
+ 
+ 
+}
